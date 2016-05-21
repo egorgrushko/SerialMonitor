3b6edd0e2a1f13823468e2c860f2c948f519776d
egorgrushko <egorgrushko@mail.ru>
2016-05-21 22:34:38 +0300

    Remove unnecessary code

--- Serial Monitor/SerialMonitorControl.xaml.cs
+++ Serial Monitor/SerialMonitorControl.xaml.cs
@@ -4,7 +4,6 @@ using System.Text;
 using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Documents;
 using System.Windows.Media;
 using System.Windows.Threading;
 
